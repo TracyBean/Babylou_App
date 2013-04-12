bc6c14fd950a4aa88fa4bc6530231777277e5735
Tracy Moscato <trcmoscato@gmail.com>
2013-04-11 22:23:07 -0400

    Babylou_App

--- main.py
+++ main.py
@@ -7,3 +7,7 @@ class Main(flask.views.MethodView):
     	if os.path.isfile('templates/' + page):
     		return flask.render_template(page)
     	flask.abort(404)
+
+
+ port = int(os.environ.get('PORT', 5000))
+ app.run(host='0.0.0.0', port=port)
